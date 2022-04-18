793d530803ceb2b186d42bdd5bcaa7690a91bf7a
clakg <clakg@icloud.com>
2022-04-18 23:29:55 +0200

    add utils folder for firebase config et .env.local

---
+++ gitter/utils/firebase.config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+
+const app = firebase.initializeApp({
+    apiKey: process.env.GITTER_APP_API_KEY,
+    authDomain: "gitter-5726a.firebaseapp.com",
+    projectId: "gitter-5726a",
+    storageBucket: "gitter-5726a.appspot.com",
+    messagingSenderId: "682986901394",
+    appId: "1:682986901394:web:611a8eef207f9465703b4b"
+});
+
+export const auth = app.auth();
+export default app;
